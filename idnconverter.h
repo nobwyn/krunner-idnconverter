9c075fbe94a37c0892af931e07fd918018261f27
Laimonas Selenis <laimoncijus@users.noreply.github.com>
2016-09-24 21:47:32 +0200

    Improves domain keyword detection

--- idnconverter.h
+++ idnconverter.h
@@ -13,12 +13,32 @@ class IdnConverter : public QObject
 public:
 	IdnConverter();
 	~IdnConverter();
+	
+	/**
+	 * Converts the given domain name to ACE format.
+	 */
 	QString convertToAce(const QString &);
+	
+	/**
+	 * Converts the given domain name to IDN format.
+	 */
 	QString convertToIdn(const QString &);
 	
 private: 
+	
+	/**
+	 * The IDNA instance for conversion
+	 */
 	icu::IDNA *idnaConv;
-	QString buildResult(const icu::UnicodeString &, const icu::IDNAInfo &, const UErrorCode &);
+	
+	/**
+	 * Builds a result string taking care of conversion errors check.
+	 * 
+	 * @param text conversion result to use
+	 * @param info container of IDNA processing details
+     * @param errCode ICU error code
+	 */
+	QString buildResult(const icu::UnicodeString &text, const icu::IDNAInfo &info, const UErrorCode &errCode);
 	
 };
 
